f706ae96598590e3703d87cfc33676ee7857c1e2
OlgaSolovieva88 <greyfireolga@mail.ru>
2024-01-18 21:59:48 +0300

    basic index template

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask 
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 @app.route('/') 
-def hello() -> str: 
-    return 'Hello world from Flask!'
+def index() -> str: 
+    return render_template('index.html')
  
 app.run()
