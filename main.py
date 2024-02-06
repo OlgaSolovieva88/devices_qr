3c6b93d293550ffca5c98cf2a38d4b4ee39cbe64
OlgaSolovieva88 <greyfireolga@mail.ru>
2024-02-06 21:53:18 +0300

    new-view

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 @app.route('/') 
 def index() -> str: 
     return render_template('index.html')
+
+@app.route('/device/<id>') 
+def device(id) -> str: 
+    return render_template('device.html')
  
 app.run()
